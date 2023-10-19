0319c1ccf432e9799e44192823e861f300393bbf
Tyler Ingersoll <tyler@ingersoll.dev>
2023-10-19 00:00:00 -0400

    add test for content store

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,48 @@
 import { defineStore } from "pinia";
-const contentful = require("contentful");
+import { createClient } from "contentful";
 
 export const useContentStore = defineStore("content", {
   state: () => ({ content: [] }),
-  actions: {
-    loadContent() {
-      const useLocalJson = () => {
-        const jsonData = require("@/data/content.json");
-        this.content = jsonData.content;
-      };
 
-      const client = contentful.createClient({
+  actions: {
+    async loadContent() {
+      if (process.env.NODE_ENV === "production") {
+        try {
+          const client = this.createContentfulClient();
+          const entry = await client.getEntry(
+            process.env.VUE_APP_CONTENTFUL_ENTRY
+          );
+          this.content = entry.fields.siteContent.content;
+        } catch (error) {
+          console.error("Failed to fetch content from Contentful:", error);
+          await this.useLocalJson();
+        }
+      } else {
+        await this.useLocalJson();
+      }
+    },
+
+    createContentfulClient() {
+      if (
+        !process.env.VUE_APP_CONTENTFUL_SPACE ||
+        !process.env.VUE_APP_CONTENTFUL_ENVIRONMENT ||
+        !process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN
+      ) {
+        throw new Error(
+          "Missing Contentful configuration in environment variables"
+        );
+      }
+
+      return createClient({
         space: process.env.VUE_APP_CONTENTFUL_SPACE,
         environment: process.env.VUE_APP_CONTENTFUL_ENVIRONMENT,
         accessToken: process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN,
       });
+    },
 
-      if (process.env.NODE_ENV === "production") {
-        client
-          .getEntry(process.env.VUE_APP_CONTENTFUL_ENTRY)
-          .then((entry) => (this.content = entry.fields.siteContent.content))
-          .catch(() => {
-            useLocalJson();
-          });
-      } else {
-        useLocalJson();
-      }
+    async useLocalJson() {
+      const jsonData = await import("@/data/content.json");
+      this.content = jsonData.content;
     },
   },
 });
